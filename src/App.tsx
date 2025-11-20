220fcbb346c791e88ae688d93c413e55050bcb6d
cxq <1667491170@qq.com>
2025-11-20 14:44:15 +0800

    init: 项目初始化

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+	return (
+		<>
+			<div>
+				<span className="text-3xl font-bold underline">Hello world!</span>
+			</div>
+			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
