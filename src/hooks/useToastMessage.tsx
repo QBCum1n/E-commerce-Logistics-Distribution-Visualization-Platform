60ec88cd606c39bebb6053d2fe9d8d275a385392
cxq <1667491170@qq.com>
2025-11-26 16:28:33 +0800

    perf: 补充消息提示hooks使用说明

--- src/hooks/useToastMessage.tsx
+++ src/hooks/useToastMessage.tsx
@@ -1,3 +1,15 @@
+/** 
+ * 消息提示钩子
+ * 用于在组件中显示消息提示
+ * const { toastMessage, contextHolder } = useToastMessage();
+ * toastMessage("success", "操作成功");
+ * return (
+ *   <div>
+ *     {contextHolder}
+ *     <Button onClick={() => toastMessage("success", "操作成功")}>操作成功</Button>
+ *   </div>
+ * );
+ */
 import { message } from "antd";
 import { useCallback } from "react";
 
